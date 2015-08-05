09f05cc0ea0932acd44485e4a595674e3da799cc
Adi Ghidel <marius-adrian.ghidel@thalesgroup.com>
2015-08-05 11:23:20 +0300

    bmotion components

---
+++ tools/bmotion/script.js
@@ -0,0 +1,35 @@
+
+requirejs(['bmotion.template'], function (bms) {
+    
+		
+	bms.executeEvent({
+		  selector: "[class='operand']",
+		  events: [
+		   		{
+		   		  name: "op2", 
+		   		  predicate: function (origin) {
+					var viewId = origin.attr("id").split("."); 
+					var opr = viewId[0];
+					var operationName = viewId[1];
+					
+					console.log("### sendEWIXL: " + opr + "." + operationName);
+					return "xx=" + opr;
+		   		  }
+		   		},
+				{
+		   		  name: "op2", 
+		   		  predicate: function (origin) {
+					var viewId = origin.attr("id").split("."); 
+					var opr = viewId[0];
+					var operationName = viewId[1];
+					
+					console.log("### sendEWIXL: " + opr + "." + operationName);
+					return "xx=" + opr;
+		   		  }
+		   		}
+		   	  ]
+	});
+	
+	
+});
+
