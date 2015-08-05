d168d7486606e4c0b822b842a08c8168f2ed08e3
Adi Ghidel <marius-adrian.ghidel@thalesgroup.com>
2015-08-05 12:21:40 +0300

    updated bmotion components for v3

--- tools/bmotion/script.js
+++ tools/bmotion/script.js
@@ -3,10 +3,10 @@ requirejs(['bmotion.template'], function (bms) {
     
 		
 	bms.executeEvent({
-		  selector: "[class='operand']",
+		  selector: "[class='notworkingop']",
 		  events: [
 		   		{
-		   		  name: "op2", 
+		   		  name: "issueOp", 
 		   		  predicate: function (origin) {
 					var viewId = origin.attr("id").split("."); 
 					var opr = viewId[0];
@@ -17,7 +17,61 @@ requirejs(['bmotion.template'], function (bms) {
 		   		  }
 		   		},
 				{
-		   		  name: "op2", 
+		   		  name: "issueOp", 
+		   		  predicate: function (origin) {
+					var viewId = origin.attr("id").split("."); 
+					var opr = viewId[0];
+					var operationName = viewId[1];
+					
+					console.log("### values: " + opr + "." + operationName);
+					return "xx=" + opr;
+		   		  }
+		   		}
+		   	  ]
+	});
+	bms.executeEvent({
+		  selector: "[class='workingop1']",
+		  events: [
+		   		{
+		   		  name: "workingOp", 
+		   		  predicate: function (origin) {
+					var viewId = origin.attr("id").split("."); 
+					var opr = viewId[0];
+					var operationName = viewId[1];
+					
+					console.log("### values: " + opr + "." + operationName);
+			
+		   		  }
+		   		},
+				{
+		   		  name: "workingOp", 
+		   		  predicate: function (origin) {
+					var viewId = origin.attr("id").split("."); 
+					var opr = viewId[0];
+					var operationName = viewId[1];
+					
+					console.log("### values: " + opr + "." + operationName);
+
+		   		  }
+		   		}
+		   	  ]
+	});
+	bms.executeEvent({
+		  selector: "[class='workingop2']",
+		  events: [
+		   		{
+		   		  name: "workingOp2", 
+		   		  predicate: function (origin) {
+					var viewId = origin.attr("id").split("."); 
+					var opr = viewId[0];
+					var operationName = viewId[1];
+					
+					console.log("### values: " + opr + "." + operationName);
+					return "xx=" + opr;
+		   		  }
+		   		},
+				{
+		   		  name: "workingOp2", 
 		   		  predicate: function (origin) {
 					var viewId = origin.attr("id").split("."); 
 					var opr = viewId[0];
