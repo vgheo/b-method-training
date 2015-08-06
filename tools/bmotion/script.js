c655c15af3f1b029a04036d39d2ffe52bd4c6275
Adi Ghidel <marius-adrian.ghidel@thalesgroup.com>
2015-08-06 10:54:42 +0300

    simplified svg and script for issue

--- tools/bmotion/script.js
+++ tools/bmotion/script.js
@@ -3,87 +3,58 @@ requirejs(['bmotion.template'], function (bms) {
     
 		
 	bms.executeEvent({
-		  selector: "[class='issueop']",
+		  selector: "#1",
 		  events: [
 		   		{
 		   		  name: "issueOp", 
 		   		  predicate: function (origin) {
-					var viewId = origin.attr("id").split("."); 
+					var viewId = origin.attr("id");
 					var opr = viewId[0];
-					var operationName = viewId[1];
-					
-					console.log("### values: " + opr + "." + operationName);
 					return "xx=" + opr;
 		   		  }
 		   		},
 				{
 		   		  name: "issueOp", 
-		   		  predicate: function (origin) {
-					var viewId = origin.attr("id").split("."); 
+				  predicate: function (origin) {
+					var viewId = origin.attr("id");
 					var opr = viewId[0];
-					var operationName = viewId[1];
-					
-					console.log("### values: " + opr + "." + operationName);
 					return "xx=" + opr;
 		   		  }
 		   		}
 		   	  ]
 	});
 	bms.executeEvent({
-		  selector: "[class='workingop']",
+		  selector: "#2",
 		  events: [
 		   		{
-		   		  name: "workingOp", 
+		   		  name: "issueOp2", 
 		   		  predicate: function (origin) {
-					var viewId = origin.attr("id").split("."); 
+					var viewId = origin.attr("id");
 					var opr = viewId[0];
-					var operationName = viewId[1];
-					
-					console.log("### values: " + opr + "." + operationName);
-			
+					return "xx=" + opr;
 		   		  }
 		   		},
 				{
-		   		  name: "workingOp", 
+		   		  name: "issueOp2", 
 		   		  predicate: function (origin) {
-					var viewId = origin.attr("id").split("."); 
+					var viewId = origin.attr("id");
 					var opr = viewId[0];
-					var operationName = viewId[1];
-					
-					console.log("### values: " + opr + "." + operationName);
-
+					return "xx=" + opr;
 		   		  }
 		   		}
 		   	  ]
 	});
 	bms.executeEvent({
-		  selector: "[class='issueop2']",
+		  selector: "#3",
 		  events: [
 		   		{
-		   		  name: "issueOp2", 
-		   		  predicate: function (origin) {
-					var viewId = origin.attr("id").split("."); 
-					var opr = viewId[0];
-					var operationName = viewId[1];
-					
-					console.log("### values: " + opr + "." + operationName);
-					return "xx=" + opr;
-		   		  }
+		   		  name: "workingOp", 
 		   		},
 				{
-		   		  name: "issueOp2", 
-		   		  predicate: function (origin) {
-					var viewId = origin.attr("id").split("."); 
-					var opr = viewId[0];
-					var operationName = viewId[1];
-					
-					console.log("### values: " + opr + "." + operationName);
-					return "xx=" + opr;
-		   		  }
-		   		}
+		   		  name: "workingOp", 
+		   		 }
 		   	  ]
 	});
-	
 	
 });
 
