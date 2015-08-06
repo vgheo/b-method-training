0c00eaab80299ca7194915fef9eeba54e1cfe5b5
vgheorgh <vlad.gheorghe@thalesgroup.com>
2015-08-06 16:08:28 +0300

    factorize event handlers - TODO - use it..

--- tools/bmotion/script.js
+++ tools/bmotion/script.js
@@ -1,4 +1,28 @@
 
+
+/*
+ * 
+ *   xx={origin.id}
+ */
+function predicateXXEquals(origin) {
+	return "xx=" + origin.attr("id");
+}
+
+function registerOpEvent(bms, id, opName) {
+	
+	bms.executeEvent({
+		  selector: "#" + id ,
+		  events: [
+		   		{ name: opName, predicate: predicateXXEquals },
+				{ name: opName,  predicate: predicateXXEquals } // force tooltip to appear..
+		   	  ]
+	});
+	
+}  
+
+// TODO - use factorization ..
+
+
 requirejs(['bmotion.template'], function (bms) {
     
 		
