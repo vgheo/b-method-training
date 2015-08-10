fe7d92af6ef76f28dade32be889c05908358ce04
Iulian Pana <iulian.pana@thalesgroup.com>
2015-08-10 11:51:08 +0300

    first code generation exa,ple -> header files

---
+++ atelierb/DatabaseSystem/lang/c/BASIC_COMMAND.h
@@ -0,0 +1,41 @@
+#ifndef _BASIC_COMMAND_h
+#define _BASIC_COMMAND_h
+
+#include <stdint.h>
+#include <stdbool.h>
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+/* Clause SETS */
+typedef enum
+{
+    BASIC_COMMAND__new,
+    BASIC_COMMAND__birth,
+    BASIC_COMMAND__marriage,
+    BASIC_COMMAND__death,
+    BASIC_COMMAND__print,
+    BASIC_COMMAND__quit
+    
+} BASIC_COMMAND__COMMAND;
+
+/* Clause CONCRETE_VARIABLES */
+
+
+/* Clause CONCRETE_CONSTANTS */
+/* Basic constants */
+/* Array and record constants */
+extern void BASIC_COMMAND__INITIALISATION(void);
+
+/* Clause OPERATIONS */
+
+extern void BASIC_COMMAND__COMMAND_READ(BASIC_COMMAND__COMMAND *rr);
+extern void BASIC_COMMAND__COMMAND_WRITE(BASIC_COMMAND__COMMAND ii);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+#endif /* _BASIC_COMMAND_h */
