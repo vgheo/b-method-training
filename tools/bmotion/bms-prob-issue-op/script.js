04937f880cc98dfc959f1dda221033c2084b3a21
Adi Ghidel <marius-adrian.ghidel@thalesgroup.com>
2015-08-10 10:45:08 +0300

    script refactorization

--- tools/bmotion/bms-prob-issue-op/script.js
+++ tools/bmotion/bms-prob-issue-op/script.js
@@ -1,5 +1,3 @@
-
-
 /*
  * 
  *   xx={origin.id}
@@ -20,63 +18,16 @@ function registerOpEvent(bms, id, opName) {
 	
 }  
 
-// TODO - use factorization ..
-
-
 requirejs(['bmotion.template'], function (bms) {
     
-		
-	bms.executeEvent({
-		  selector: "#1",
-		  events: [
-		   		{
-		   		  name: "issueOp", 
-		   		  predicate: function (origin) {
-					var viewId = origin.attr("id");
-					var opr = viewId[0];
-					return "xx=" + opr;
-		   		  }
-		   		},
-				{
-		   		  name: "issueOp", 
-				  predicate: function (origin) {
-					var viewId = origin.attr("id");
-					var opr = viewId[0];
-					return "xx=" + opr;
-		   		  }
-		   		}
-		   	  ]
-	});
-	bms.executeEvent({
-		  selector: "#2",
-		  events: [
-		   		{
-		   		  name: "issueOp2", 
-		   		  predicate: function (origin) {
-					var viewId = origin.attr("id");
-					var opr = viewId[0];
-					return "xx=" + opr;
-		   		  }
-		   		},
-				{
-		   		  name: "issueOp2", 
-		   		  predicate: function (origin) {
-					var viewId = origin.attr("id");
-					var opr = viewId[0];
-					return "xx=" + opr;
-		   		  }
-		   		}
-		   	  ]
-	});
+	registerOpEvent (bms, "1" , "issueOp");
+	registerOpEvent (bms, "2" , "issueOp2");
+
 	bms.executeEvent({
 		  selector: "#3",
 		  events: [
-		   		{
-		   		  name: "workingOp", 
-		   		},
-				{
-		   		  name: "workingOp", 
-		   		 }
+		   		{ name: "workingOp"},
+				{ name: "workingOp"}
 		   	  ]
 	});
 	
