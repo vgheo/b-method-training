a0adb64b7eea0dbb817aced12ee898ad9382c61f
Simona Roboiu <simona-alina.roboiu@thalesgroup.com>
2015-09-01 11:25:59 +0300

    add path for read and write for translator

--- TestCaseGeneration/TranslatorForTestCaseGeneration/translatorProB_cppUnit/src/convertXmlProBToCppUnit/Const.java
+++ TestCaseGeneration/TranslatorForTestCaseGeneration/translatorProB_cppUnit/src/convertXmlProBToCppUnit/Const.java
@@ -9,8 +9,9 @@ public class Const {
 	
 	public static final String templateComment = "template_comment";
 	public static final String testClassName = "TestDataChannelFunctionality";
-	public static final String localPathForRead = "D:\\Users\\T0164929\\_ProjectsGit\\b-method-training\\TestCaseGeneration\\DB_test_case_generation\\tc-gen\\";
-	public static final String outputUnitTest = testClassName + ".cpp";
-	public static final String XMLTestFile = "test_cbc.xml";
+	
+	public static final String pathTCG = ".\\..\\..\\DB_test_case_generation\\";
+	public static final String outputUnitTest = pathTCG + "tc-gen\\" + testClassName + ".cpp";
+	public static final String XMLTestFile = pathTCG + "prob\\" + "test_cbc.xml";
 	
 }
