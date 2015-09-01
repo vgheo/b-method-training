e697926a0484971a3fee54cfccb08412797761b6
Simona Roboiu <simona-alina.roboiu@thalesgroup.com>
2015-09-01 11:09:54 +0300

    generate output

--- TestCaseGeneration/TranslatorForTestCaseGeneration/translatorProB_cppUnit/src/convertXmlProBToCppUnit/Const.java
+++ TestCaseGeneration/TranslatorForTestCaseGeneration/translatorProB_cppUnit/src/convertXmlProBToCppUnit/Const.java
@@ -9,6 +9,7 @@ public class Const {
 	
 	public static final String templateComment = "template_comment";
 	public static final String testClassName = "TestDataChannelFunctionality";
+	public static final String localPathForRead = "D:\\Users\\T0164929\\_ProjectsGit\\b-method-training\\TestCaseGeneration\\DB_test_case_generation\\tc-gen\\";
 	public static final String outputUnitTest = testClassName + ".cpp";
 	public static final String XMLTestFile = "test_cbc.xml";
 	
