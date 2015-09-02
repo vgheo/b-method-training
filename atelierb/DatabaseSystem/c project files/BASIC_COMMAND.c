98f42d4c0c63c9eb8e8cee9c2a64bac2753344f3
Iulian Pana <iulian.pana@thalesgroup.com>
2015-09-02 15:36:57 +0300

    loop problem solved

--- atelierb/DatabaseSystem/c project files/BASIC_COMMAND.c
+++ atelierb/DatabaseSystem/c project files/BASIC_COMMAND.c
@@ -6,14 +6,23 @@
 	 	
   }
   void BASIC_COMMAND__COMMAND_READ(BASIC_COMMAND__COMMAND *rr)
-  {		char input[20];
+  {		
+  		char input[20];
   
 	 	printf("\n[COMMAND_READ]:Insert command :\n(new:0,birth:1,marriage:2,death:3,print:4,quit:5)\n");
 	 	scanf("%s",input);	
-	 	
+		 	 	
 	 	*rr = atoi(input);
-	 	if((( *rr) < 0 || (*rr)>5) && (strcmp(input,"0"))!=0)
+	 	 /*If command is a number it must be in[0,5] interval.*/
+	 	if( ( ( *rr) < 0 || (*rr)>5) )
 	 	{ 
+		  printf("[COMMAND_READ]:INVALID COMMAND!\nExpected value in [0,5] interval.\n") ;
+		  *rr = INT_MAX;
+		}
+		
+		//if atoi returns 0 it is possible that the string couldn't be parsed as a number
+		if(atoi(input) == 0 && strcmp(strtok(input,"\n"),"0") != 0 )
+		{ 
 		  printf("[COMMAND_READ]:INVALID COMMAND!\nExpected value in [0,5] interval.\n") ;
 		  *rr = INT_MAX;
 		}
